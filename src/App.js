12e6552dfdf64ddfff3792ca5ae714198ab4b2b1
Shidorama <shido.spb@gmail.com>
2020-03-07 00:15:23 +0000

    Initial commit, basic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+import './App.css';
+import * as PropTypes from "prop-types";
+
+class ExampleToast extends React.Component {
+    render() {
+        let {children} = this.props;
+        const [show, toggleShow] = useState(true);
+
+        return (
+            <>
+                {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+                <Toast show={show} onClose={() => toggleShow(false)}>
+                    <Toast.Header>
+                        <strong className="mr-auto">React-Bootstrap</strong>
+                    </Toast.Header>
+                    <Toast.Body>{children}</Toast.Body>
+                </Toast>
+            </>
+        );
+    }
+}
+
+ExampleToast.propTypes = {children: PropTypes.any}
+
+class App extends React.Component {
+    render() {
+        return (
+            <Profile>
+
+            </Profile>
+        );
+    }
+}
+
+class Profile extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            dataLoaded: false
+        }
+        this.profile = ""
+        // this.props = {
+        //   p
+        // }
+    }
+
+    componentDidMount() {
+        if (!this.state.dataLoaded) {
+            fetch("http://localhost:5000/me").then(
+                result => result.json()
+            ).then(
+                data => {
+                    console.log("Got data")
+                    console.log(data)
+                    this.profile = data;
+                    this.setState({"dataLoaded": true});
+                }
+            )
+        }
+    }
+
+    render() {
+        return (
+            <Container>
+              {this.state.dataLoaded && this.profile.name}
+            </Container>
+        )
+    }
+
 }
 
 export default App;
